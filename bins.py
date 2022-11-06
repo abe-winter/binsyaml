7b6d69ab030e91dd735dd2f2ac75f1eebbcba989
Abe Winter <awinter.public@gmail.com>
2022-11-05 23:30:33 -0400

    extract=rename, fix null version flag

--- bins.py
+++ bins.py
@@ -2,7 +2,7 @@
 "load bins.json and install missing files"
 
 import argparse, subprocess, json, dataclasses, logging, os, tarfile, re
-from typing import Optional
+from typing import Optional, Literal
 
 logger = logging.getLogger(__name__)
 
@@ -11,11 +11,16 @@ def download(url):
     subprocess.run(['wget', url])
     logger.info('ok dl %s', url)
 
-def install(bin_name, archive_name, dest):
+def install(bin_name, archive_name, dest, extract):
     extensions = archive_name.split('.')
-    if 'tar' in extensions or 'tgz' in extensions:
+    if extract == 'tar' and ('tar' in extensions or 'tgz' in extensions):
+        logger.debug('installing %s in %s from archive %s', bin_name, dest, archive_name)
         tarfile.open(archive_name).extract(bin_name)
         subprocess.run(['sudo', 'install', bin_name, dest])
+    elif extract == 'rename':
+        logger.debug('installing %s in %s from archive %s', bin_name, dest, archive_name)
+        subprocess.run(['chmod', '+x', archive_name])
+        subprocess.run(['sudo', 'install', archive_name, os.path.join(dest, bin_name)])
     else:
         raise ValueError('unknown extension type in', archive_name)
 
@@ -24,8 +29,9 @@ class Spec:
     name: str
     url: str
     version: Optional[str] = None
-    version_flag: str = '--version'
+    version_flag: Optional[str] = None
     version_regex: Optional[str] = None
+    extract: Literal['tar', 'rename'] = 'tar'
 
     def dl_url(self):
         return self.url.format(version=self.version) \
@@ -40,7 +46,7 @@ class Spec:
 
     def installed_version(self, dest: str):
         "return installed version of tool"
-        if not self.bin_exists(dest):
+        if self.version_flag is None or not self.bin_exists(dest):
             return None
         ret = subprocess.run([os.path.join(dest, self.name), self.version_flag], capture_output=True)
         return re.search(self.version_regex, ret.stdout.decode()).groups()[0] if self.version_regex else ret.stdout.strip()
@@ -79,7 +85,7 @@ def main():
         else:
             download(spec.dl_url())
         if not args.noinstall:
-            install(spec.name, spec.dl_target(), args.dest)
+            install(spec.name, spec.dl_target(), args.dest, spec.extract)
         if args.clean:
             raise NotImplementedError('todo: delete archives')
 
